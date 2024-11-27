a82ff3552f961bcfb83d617060ad1b042b850e6e
Zhyhmant <103449586+Zhyhmant@users.noreply.github.com>
2024-11-27 11:06:19 +0300

    NextTomorrow

--- src/Lesson2/JavaHomework.java
+++ src/Lesson2/JavaHomework.java
@@ -1,4 +1,36 @@
 package Lesson2;
 
+import java.sql.SQLOutput;
+
 public class JavaHomework {
-}
+    public static void main(String[] args) {
+        enterTask4();
+    }
+        public static void enterTask() {
+            int valueVeriableLow = 10;
+            int valueVeriableHigh = 15;
+            System.out.print(4 * (valueVeriableLow + valueVeriableHigh - 1) / 2);
+        }
+
+        public static void enterNextTask(){
+            int valueNumberOut = 15;
+            int valueMin = 1;
+            int valueMax = 5;
+            System.out.print(valueMin + valueMax);
+            }
+        public static void enterTask3(){
+         int valueNamberOut = 159;
+         int valueMeaningMin = 1;
+         int valueMeaningMid = 5;
+         int valueMeaningMax = 9;
+         System.out.print(valueMeaningMin + valueMeaningMid + valueMeaningMax);
+           }
+
+        public static void enterTask4(){
+         float valueNumber = 30.5F;
+         int valueNumber = 30
+         System.out.print(valueNumber );
+        }
+        }
+
+
