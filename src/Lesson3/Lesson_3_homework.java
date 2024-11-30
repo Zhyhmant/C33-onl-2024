02c701be9d36a7703567e169bccc289b845bf54c
Zhyhmant <103449586+Zhyhmant@users.noreply.github.com>
2024-11-30 13:20:34 +0300

    Next later

--- src/Lesson3/Lesson_3_homework.java
+++ src/Lesson3/Lesson_3_homework.java
@@ -1,18 +1,71 @@
 package Lesson3;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Lesson_3_homework {
     public static void main(String[] args) {
-        enterTask1();
+        enterTask4();
     }
 
     public static void enterTask1() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number :");
-        String numer = input.nextLine();
-        System.out.print("True or false :");
-        String truse = input.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please,enter number: ");
+        if (scanner.hasNext()) {
+            int number = scanner.nextInt();
+            if (number % 2 == 0) {
+                System.out.println(number + " - An even number");
+            } else {
+                System.out.println(number + " - Ann odd number");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void enterTask2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the air temperature:");
+        if (scanner.hasNext()) {
+            int temperature = scanner.nextInt();
+            {
+                if (temperature > -5) {
+                    System.out.println("WARM");
+                } else {
+                    if (temperature > -20) {
+                        System.out.println("NORMAL");
+                    } else {
+                        if (temperature - 20 > temperature) ;
+                        System.out.println("COLD");
+                    }
+                }
+                scanner.close();
+            }
+        }
+    }
+
+    public static void enterTask3() {
+        for (int square = 10; square <= 20; square++) {
+            System.out.println("the square of the number " + square + " equal to " + (square * square));
+        }
+    }
+
+    public static void enterTask4() {
+        int beginnin = 7;
+        while (beginnin <= 98) {
+            System.out.print(beginnin + " ");
+            beginnin += 7;
+        }
+    }
+
+    public static void enterTask5(){
+
     }
 }
+
+
+
+
+
+
+
+
+
