249c48934808f145967b5374ad0a45f6c6e163c3
Zhyhmant <103449586+Zhyhmant@users.noreply.github.com>
2024-12-03 15:30:16 +0300

    TomorrowTwo

--- src/Lesson4/Homework_4_lesson.java
+++ src/Lesson4/Homework_4_lesson.java
@@ -2,33 +2,33 @@ package Lesson4;
 
 public class Homework_4_lesson {
     public static void main(String[] arg) {
-        enterTask2();
+        enterTak4();
     }
 
     public static void useTest() {
         int[] arrayOne = new int[10];
-        arrayOne[0]= 30;
-        arrayOne[1]= 29;
-        arrayOne[2]= 28;
-        arrayOne[3]= 27;
-        arrayOne[4]= 26;
-        arrayOne[5]= 25;
-        arrayOne[6]= 24;
-        arrayOne[7]= 23;
-        arrayOne[8]= 22;
-        arrayOne[9]= 21;
+        arrayOne[0] = 30;
+        arrayOne[1] = 29;
+        arrayOne[2] = 28;
+        arrayOne[3] = 27;
+        arrayOne[4] = 26;
+        arrayOne[5] = 25;
+        arrayOne[6] = 24;
+        arrayOne[7] = 23;
+        arrayOne[8] = 22;
+        arrayOne[9] = 21;
         System.out.println("First number : " + arrayOne[4] + " result");
 
     }
 
     public static void useTestTwo() {
-      int[] numbers = new int[]{1,2,3,4,5,6,7,8,9};
-      String[] names = new String[]{"Dzimitry","Zhyhhmant",};
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        String[] names = new String[]{"Dzimitry", "Zhyhhmant",};
         System.out.println("My Name :" + names[0] + " " + names[1]);
         System.out.println("Age: " + numbers[2] + numbers[0]);
     }
 
-    public static void enterTask1(){
+    public static void enterTask1() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.print("The direct order : ");
@@ -42,7 +42,8 @@ public class Homework_4_lesson {
         }
         System.out.println();
     }
-    public static void enterTask2(){
+
+    public static void enterTask2() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int min = array[0];
         int max = array[0];
@@ -58,8 +59,28 @@ public class Homework_4_lesson {
         System.out.println("Maximum element: " + max);
 
     }
-    public static void enterTask3(){
+
+    public static void enterTask3() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
+        int minIndex = 0;
+        int maxIndex = 0;
+        for (int number = 1; number < array.length; number++) {
+            if (array[number] < array[minIndex]) {
+                minIndex = number;
+            }
+            if (array[number] > array[maxIndex]) {
+                maxIndex = number;
+            }
+        }
+
+        System.out.println("Minimum index : " + minIndex);
+        System.out.println("Maximum index: " + maxIndex);
+    }
+
+    public static void enterTak4(){
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
 
     }
 }
+
 
